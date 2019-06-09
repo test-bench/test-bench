6a2b05399845636503c0f59aa00caa3bd733518c
Nathan Ladd <nathanladd+github@gmail.com>
2019-06-09 17:49:50 -0500

    Controls: Time

---
+++ lib/test_bench/controls/time.rb
@@ -0,0 +1,71 @@
+module TestBench
+  module Controls
+    module Time
+      def self.example(seconds_offset: nil)
+        seconds_offset ||= 0
+
+        year = self.year
+        month = self.month
+        day = self.day
+
+        hours = self.hours
+        minutes = self.minutes
+        seconds = self.seconds + seconds_offset
+
+        if not RUBY_ENGINE == 'mruby'
+          tz_offset = self.tz_offset
+
+          final_argument = tz_offset
+        else
+          seconds, subseconds = seconds.divmod(1)
+
+          microseconds = subseconds * 1_000_000
+
+          final_argument = microseconds
+        end
+
+        ::Time.new(year, month, day, hours, minutes, seconds, final_argument)
+      end
+
+      def self.year
+        2000
+      end
+
+      def self.month
+        1
+      end
+
+      def self.day
+        1
+      end
+
+      def self.hours
+        11
+      end
+
+      def self.minutes
+        11
+      end
+
+      def self.seconds
+        11.0
+      end
+
+      def self.tz_offset
+        0
+      end
+
+      module Elapsed
+        def self.example
+          1.111
+        end
+
+        module Text
+          def self.example
+            "1.111s"
+          end
+        end
+      end
+    end
+  end
+end
