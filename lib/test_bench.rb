2d122b534d2d09ff7300b5c1e2d055e25805309d
Nathan Ladd <nathanladd+github@gmail.com>
2019-07-09 10:56:43 -0500

    Output (Timer): Start

--- lib/test_bench.rb
+++ lib/test_bench.rb
@@ -7,3 +7,5 @@ require 'test_bench/environment/boolean'
 require 'test_bench/output/writer'
 require 'test_bench/output/writer/sgr'
 require 'test_bench/output/writer/substitute'
+
+require 'test_bench/output/timer'
