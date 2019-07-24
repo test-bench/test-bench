975aadce299fc485e2cd8abd2d9c9364c831c24f
Nathan Ladd <nathanladd+github@gmail.com>
2019-07-24 17:17:42 -0500

    Toplevel Module: Evaluate

--- lib/test_bench/test_bench.rb
+++ lib/test_bench/test_bench.rb
@@ -29,6 +29,14 @@ module TestBench
     fixture(session, receiver: receiver)
   end
 
+  def self.evaluate(session: nil, &block)
+    fixture = fixture(session)
+
+    fixture.context do
+      fixture.instance_exec(&block)
+    end
+  end
+
   def self.fixture(session=nil, receiver: nil)
     session ||= self.session
     receiver ||= Object.new
