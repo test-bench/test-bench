8ba85fb257b81d68322820b4aab44819e53f47f2
Nathan Ladd <nathanladd+github@gmail.com>
2019-07-25 12:05:07 -0500

    CLI (Argument Parser): Output Styling

---
+++ test/automated/cli/parse_arguments/output_styling.rb
@@ -0,0 +1,49 @@
+require_relative '../../automated_init'
+
+context "CLI" do
+  context "Parse Arguments" do
+    context "Output Styling" do
+      context "Short Form" do
+        parse_arguments = CLI::ParseArguments.new(['-s', 'on'])
+
+        parse_arguments.()
+
+        test do
+          assert(parse_arguments.writer.styling?)
+        end
+      end
+
+      context "Long Form" do
+        parse_arguments = CLI::ParseArguments.new(['--output-styling'])
+
+        parse_arguments.()
+
+        test do
+          assert(parse_arguments.writer.styling?)
+        end
+      end
+
+      context "Optional Value" do
+        context do
+          parse_arguments = CLI::ParseArguments.new(['--output-styling', 'off'])
+
+          parse_arguments.()
+
+          test do
+            refute(parse_arguments.writer.styling?)
+          end
+        end
+
+        context "Invalid Text" do
+          parse_arguments = CLI::ParseArguments.new(['--output-styling', 'invalid'])
+
+          test "Raises an error" do
+            assert_raises(Output::Writer::Error) do
+              parse_arguments.()
+            end
+          end
+        end
+      end
+    end
+  end
+end
