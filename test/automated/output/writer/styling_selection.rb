712a0ecc2271f2634f9337d0b1a7668ec64a8ad8
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-03 17:08:55 -0500

    Output (Writer): Styling Selection

---
+++ test/automated/output/writer/styling_selection.rb
@@ -0,0 +1,81 @@
+require_relative '../../automated_init'
+
+context "Output" do
+  context "Writer" do
+    context "Styling Selection" do
+      context "Detect" do
+        styling = :detect
+
+        context do
+          writer = Output::Writer.build(styling: styling)
+
+          test "Styling attribute" do
+            assert(writer.styling == styling)
+          end
+        end
+
+        context "Interactive Device" do
+          device = Controls::Device::Interactive.example
+
+          writer = Output::Writer.build(device, styling: styling)
+
+          test "Enabled" do
+            assert(writer.styling? == true)
+          end
+        end
+
+        context "Non-Interactive Device" do
+          device = Controls::Device::Interactive::Non.example
+
+          writer = Output::Writer.build(device, styling: styling)
+
+          test "Disabled" do
+            assert(writer.styling? == false)
+          end
+        end
+      end
+
+      context "On" do
+        styling = :on
+
+        device = Controls::Device::Interactive::Non.example
+
+        writer = Output::Writer.build(device, styling: styling)
+
+        test "Styling attribute" do
+          assert(writer.styling == styling)
+        end
+
+        test "Enabled" do
+          assert(writer.styling? == true)
+        end
+      end
+
+      context "Setting: Disabled" do
+        styling = :off
+
+        device = Controls::Device::Interactive.example
+
+        writer = Output::Writer.build(device, styling: styling)
+
+        test "Styling attribute" do
+          assert(writer.styling == styling)
+        end
+
+        test "Disabled" do
+          assert(writer.styling? == false)
+        end
+      end
+
+      context "Setting: Unknown" do
+        styling = :unknown
+
+        test "Raises an error" do
+          assert_raises(Output::Writer::Error) do
+            Output::Writer.build(styling: styling)
+          end
+        end
+      end
+    end
+  end
+end
