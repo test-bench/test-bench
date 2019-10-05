821e32ac78b0f7e1c49e777c62d3241020c65fea
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-05 11:38:37 -0500

    Output (Writer): Newline

--- lib/test_bench/controls.rb
+++ lib/test_bench/controls.rb
@@ -15,4 +15,5 @@ require 'test_bench/controls/test_file'
 require 'test_bench/controls/time'
 
 require 'test_bench/controls/output/escape_code'
+require 'test_bench/controls/output/newline_character'
 require 'test_bench/controls/output/styling'
