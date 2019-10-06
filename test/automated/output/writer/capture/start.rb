7c19dc95bc3040940fcb2781abd9b4ca975cc56e
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-06 17:39:11 -0500

    Output (Writer): Start Capture

---
+++ test/automated/output/writer/capture/start.rb
@@ -0,0 +1,43 @@
+require_relative '../../../automated_init'
+
+context "Output" do
+  context "Writer" do
+    context "Capture" do
+      context "Start Capture" do
+        string = String.new
+
+        context "Not Already Capturing" do
+          writer = Output::Writer.new
+
+          previous_device = writer.device or fail
+
+          writer.start_capture(string)
+
+          test "Device is changed to a string IO" do
+            assert(writer.device.instance_of?(StringIO))
+          end
+
+          test "Previous device is saved" do
+            assert(writer.previous_device.equal?(previous_device))
+          end
+
+          test "Device is configured to write to given string" do
+            assert(writer.device.string.equal?(string))
+          end
+        end
+
+        context "Already Capturing" do
+          writer = Output::Writer.new
+
+          writer.start_capture(string)
+
+          test "Raises an error" do
+            assert_raises Output::Writer::Error do
+              writer.start_capture(string)
+            end
+          end
+        end
+      end
+    end
+  end
+end
