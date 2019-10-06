b0423159a07ae20efe0e929f34773c89c6ea4a30
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-06 17:59:30 -0500

    Output (Writer Substitute): Enable Styling

--- lib/test_bench/output/writer/substitute.rb
+++ lib/test_bench/output/writer/substitute.rb
@@ -25,6 +25,10 @@ module TestBench
               pattern
             end
           end
+
+          def enable_styling!
+            self.styling = true
+          end
         end
       end
     end
