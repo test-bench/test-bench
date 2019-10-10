edc93934fdd0f814314f35334d661f23d22fb91f
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-10 10:31:57 -0500

    Fixtures: Configure Receiver

---
+++ test/automated/fixtures/configure_receiver/configure_receiver.rb
@@ -0,0 +1,48 @@
+require_relative '../../automated_init'
+
+context "Fixtures" do
+  context "Configure Receiver" do
+    context "Pass" do
+      cls = Class.new do
+        def self.configure(receiver, attr_name: nil)
+          attr_name ||= :some_attr
+
+          instance = new
+          receiver.public_send(:"#{attr_name}=", instance)
+          instance
+        end
+      end
+
+      fixture(Fixtures::ConfigureReceiver, cls, attr_name: :some_attr)
+    end
+
+    context "Failure" do
+      context "No Configure Method" do
+        cls = Object.new
+
+        fixture = Fixtures::ConfigureReceiver.new(cls, :some_attr)
+
+        fixture.()
+
+        test do
+          assert(fixture.test_session.failed?)
+        end
+      end
+
+      context "Does not configure receiver" do
+        cls = Class.new do
+          def self.configure(receiver, attr_name: nil)
+          end
+        end
+
+        fixture = Fixtures::ConfigureReceiver.new(cls, :some_attr)
+
+        fixture.()
+
+        test do
+          assert(fixture.test_session.failed?)
+        end
+      end
+    end
+  end
+end
