8ba99dd9c08c637108e9423fbd718319243add0a
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-10 16:29:06 -0500

    Output (Files): Exit File

---
+++ test/automated/output/files/exit_file.rb
@@ -0,0 +1,50 @@
+require_relative '../../automated_init'
+
+context "Output" do
+  context "Summary" do
+    context "Exit File" do
+      path = Controls::Path.example
+      result = Controls::Result.example
+
+      context "Text Was Written Since File Was Entered" do
+        output = Output.new
+
+        output.writer.byte_offset = 11
+
+        output.previous_byte_offset = 0
+
+        output.exit_file(path, result)
+
+        test "Writes nothing" do
+          refute(output.writer.written?)
+        end
+
+        test "Previous byte offset is reset" do
+          assert(output.previous_byte_offset.nil?)
+        end
+      end
+
+      context "No Text Was Written Since File Was Entered" do
+        output = Output.new
+
+        output.writer.enable_styling!
+        output.writer.byte_offset = 11
+
+        output.previous_byte_offset = 11
+
+        output.exit_file(path, result)
+
+        test "Writes notice that no output was written" do
+          assert(output.writer.written?(<<~TEXT))
+          \e[2m(Nothing written)\e[22m
+
+          TEXT
+        end
+
+        test "Previous byte offset is reset" do
+          assert(output.previous_byte_offset.nil?)
+        end
+      end
+    end
+  end
+end
