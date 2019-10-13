79a17c9c7b479773f8b6c8f942ffc816c13ade73
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-13 16:01:15 -0500

    CLI (Runner): File

--- lib/test_bench/cli/run.rb
+++ lib/test_bench/cli/run.rb
@@ -6,10 +6,42 @@ module TestBench
       end
       attr_writer :test_run
 
-      def call
+      def exclude_file_pattern
+        @exclude_file_pattern ||= Defaults.exclude_file_pattern
+      end
+      attr_writer :exclude_file_pattern
+
+      def call(&block)
         test_run.start
 
+        block.(self) unless block.nil?
+
         test_run.finish
+      end
+
+      def path(path)
+        if File.exist?(path)
+          file(path)
+        end
+      end
+      alias_method :<<, :path
+
+      def file(path)
+        if exclude_file_pattern.match?(path)
+          return nil
+        end
+
+        test_run.load(path)
+      end
+
+      module Defaults
+        def self.exclude_file_pattern
+          pattern = ENV.fetch('TEST_BENCH_EXCLUDE_FILE_PATTERN') do
+            '_init.rb$'
+          end
+
+          Regexp.new(pattern)
+        end
       end
     end
   end
--- test/automated/cli/runner/runner.rb
+++ test/automated/cli/runner/runner.rb
@@ -2,16 +2,16 @@ require_relative '../../automated_init'
 
 context "CLI" do
   context "Runner" do
-    run = CLI::Run.new
+    runner = CLI::Run.new
 
-    run.()
+    runner.()
 
     test "Test run is started" do
-      assert(run.test_run.started?)
+      assert(runner.test_run.started?)
     end
 
     test "Test run is finished" do
-      assert(run.test_run.finished?)
+      assert(runner.test_run.finished?)
     end
   end
 end
