277411d3504050f4966857cae2937efccddb9cb1
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-14 09:57:41 -0500

    Toplevel Module: Output Setter/Getter

---
+++ test/automated/set_output.rb
@@ -0,0 +1,35 @@
+require_relative './automated_init'
+
+context "Set Output" do
+  context do
+    session = Fixture::Session.new
+
+    output = Output.build
+
+    TestBench.set_output(output, session: session)
+
+    test "Sets the session's output to the one given" do
+      assert(session.output.equal?(output))
+    end
+  end
+
+  context "Given Multiple Outputs" do
+    output_1 = Output.build
+    output_2 = Output.build
+
+    session = Fixture::Session.new
+
+    TestBench.set_output([output_1, output_2], session: session)
+
+    output = session.output or fail
+
+    test "Assigns a multiple output" do
+      assert(output.instance_of?(Fixture::Output::Multiple))
+    end
+
+    test "Assigned output writes to each given output" do
+      assert(output.registered?(output_1))
+      assert(output.registered?(output_2))
+    end
+  end
+end
