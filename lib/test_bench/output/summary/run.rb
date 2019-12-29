ce4dddf3daba1d7f4afd47e975791aaa4ea1f160
Nathan Ladd <nathanladd@gmail.com>
2019-12-29 10:54:32 -0600

    Output (Run Summary): Error

--- lib/test_bench/output/summary/run.rb
+++ lib/test_bench/output/summary/run.rb
@@ -9,6 +9,11 @@ module TestBench
         end
         attr_writer :elapsed_time
 
+        def error_count
+          @error_count ||= 0
+        end
+        attr_writer :error_count
+
         def file_count
           @file_count ||= 0
         end
@@ -31,6 +36,10 @@ module TestBench
           self.elapsed_time += elapsed_time
 
           elapsed_time
+        end
+
+        def error(error)
+          self.error_count += 1
         end
       end
     end
