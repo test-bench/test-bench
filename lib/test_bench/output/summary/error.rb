1aa698343e17852cb975bff4957ff5ab8fb9b372
Nathan Ladd <nathanladd@gmail.com>
2019-12-29 11:34:25 -0600

    Output (Error Summary): Finish

--- lib/test_bench/output/summary/error.rb
+++ lib/test_bench/output/summary/error.rb
@@ -12,6 +12,11 @@ module TestBench
           files.last
         end
 
+        def writer
+          @writer ||= Writer::Substitute.build
+        end
+        attr_writer :writer
+
         def enter_file(path)
           file = File.build(path)
 
@@ -28,6 +33,45 @@ module TestBench
           unless current_file.nil?
             current_file.error(error)
           end
+        end
+
+        def finish(_)
+          return if files.empty?
+
+          writer
+            .escape_code(:bold)
+            .escape_code(:red)
+            .text('Error Summary:')
+            .escape_code(:reset_intensity)
+            .escape_code(:reset_fg)
+            .newline
+
+          files.each do |file|
+            path, errors = file.to_a
+
+            error_count = errors.count
+
+            writer
+              .text(error_count.to_s.rjust(4, ' '))
+              .text(": #{path}")
+              .newline
+
+            errors.each do |error|
+              location = error.backtrace[0]
+
+              writer
+                .text("      ")
+                .escape_code(:red)
+                .text("#{location}: ")
+                .escape_code(:bold)
+                .text("#{error.message} (#{error.class})")
+                .escape_code(:reset_intensity)
+                .escape_code(:reset_fg)
+                .newline
+            end
+          end
+
+          writer.newline
         end
 
         File = Struct.new(:path, :errors) do
