308bb0844c1875df0162e65072cd0c675a87caad
Nathan Ladd <nathanladd@gmail.com>
2019-12-29 11:08:15 -0600

    Output (Run Summary): Finish Run

--- lib/test_bench/output/summary/run.rb
+++ lib/test_bench/output/summary/run.rb
@@ -44,6 +44,11 @@ module TestBench
         end
         attr_writer :timer
 
+        def writer
+          @writer ||= Writer::Substitute.build
+        end
+        attr_writer :writer
+
         def enter_file(_)
           timer.start
 
@@ -74,6 +79,84 @@ module TestBench
 
         def skip_test(_)
           self.skip_count += 1
+        end
+
+        def finish(result)
+          failed = !result
+
+          if elapsed_time.nonzero?
+            tests_per_second = test_count / elapsed_time
+          end
+
+          if failed
+            writer.escape_code(:red)
+          end
+
+          writer
+            .text("Finished running #{numeric_label(file_count, 'file')}")
+            .newline
+            .text("Ran %s in %.3fs (%.1f tests/second)" % [
+              numeric_label(test_count, 'test'),
+              elapsed_time,
+              tests_per_second || 0])
+            .newline
+
+          if pass_count.nonzero? && !failed
+            writer
+              .escape_code(:green)
+              .text("#{pass_count} passed")
+              .escape_code(:reset_fg)
+          else
+            writer.text("#{pass_count} passed")
+          end
+
+          writer.text(", ")
+
+          if skip_count.nonzero? && !failed
+            writer
+              .escape_code(:yellow)
+              .text("#{skip_count} skipped")
+              .escape_code(:reset_fg)
+          else
+            writer.text("#{skip_count} skipped")
+          end
+
+          writer.text(", ")
+
+          if failure_count.nonzero?
+            writer
+              .escape_code(:bold)
+              .text("#{failure_count} failed")
+              .escape_code(:reset_intensity)
+          else
+            writer.text("0 failed")
+          end
+
+          writer.text(", ")
+
+          if failed
+            writer
+              .escape_code(:bold)
+              .text(numeric_label(error_count, 'total error'))
+              .escape_code(:reset_intensity)
+              .escape_code(:reset_fg)
+          else
+            writer.text("0 total errors")
+          end
+
+          2.times do
+            writer.newline
+          end
+        end
+
+        def numeric_label(number, label, plural_text=nil)
+          plural_text ||= 's'
+
+          if number == 1
+            "#{number} #{label}"
+          else
+            "#{number} #{label}#{plural_text}"
+          end
         end
       end
     end
