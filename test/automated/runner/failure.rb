aa6a0aeb008e4b0e485ad7791cfc1471750013d9
Nathan Ladd <nathanladd@gmail.com>
2020-02-12 14:12:04 -0600

    Runner: Instance Actuator

---
+++ test/automated/runner/failure.rb
@@ -0,0 +1,22 @@
+require_relative '../automated_init'
+
+context "Runner" do
+  context "Failure" do
+    test_file = Controls::TestFile::Failure.example
+
+    runner = Run.new(test_file)
+
+    output = Output::Substitute.build
+
+    Session.configure(runner, output: output)
+
+    begin
+      runner.()
+    rescue Controls::Error::Example
+    end
+
+    test "Session is finished" do
+      assert(runner.session.finished?)
+    end
+  end
+end
