83460c8fed6b092f04d4d28c9a86c82ac33fd2af
Nathan Ladd <nathanladd@gmail.com>
2020-02-12 14:12:04 -0600

    Runner: Instance Actuator

---
+++ lib/test_bench/run.rb
@@ -0,0 +1,39 @@
+module TestBench
+  class Run
+    def session
+      @session ||= Session::Substitute.build
+    end
+    attr_writer :session
+
+    attr_reader :paths
+
+    def initialize(*paths)
+      @paths = Array(paths)
+    end
+
+    def call(&block)
+      session.start
+
+      if block.nil?
+        paths.each do |path|
+          path(path)
+        end
+      else
+        block.()
+      end
+
+    ensure
+      session.finish
+    end
+
+    def path(path)
+      if File.exist?(path)
+        file(path)
+      end
+    end
+
+    def file(path)
+      session.load(path)
+    end
+  end
+end
