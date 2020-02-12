5f85750f0b27a000970e0ef2074614deed5c9e4f
Nathan Ladd <nathanladd@gmail.com>
2020-02-12 11:40:14 -0600

    Toplevel Module: Session

---
+++ lib/test_bench/test_bench.rb
@@ -0,0 +1,35 @@
+module TestBench
+  def self.session
+    @session ||= build_session.tap do |session|
+      at_exit do
+        exit 1 if session.failed?
+      end
+    end
+  end
+  singleton_class.attr_writer(:session)
+
+  def self.build_session(output: nil, abort_on_error: nil, **args)
+    output ||= Output.build
+    error_policy = session_error_policy(abort_on_error)
+
+    Fixture::Session.build(output: output, error_policy: error_policy, **args)
+  end
+
+  def self.session_error_policy(abort_on_error=nil)
+    abort_on_error = Session::Defaults.abort_on_error if abort_on_error.nil?
+
+    if abort_on_error
+      :abort
+    else
+      :rescue
+    end
+  end
+
+  Session = Fixture::Session
+
+  module Session::Defaults
+    def self.abort_on_error
+      Environment::Boolean.fetch('TEST_BENCH_ABORT_ON_ERROR', false)
+    end
+  end
+end
