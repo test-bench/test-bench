70d68e9b01f9ea64f217f4e8e99184416fbb82a3
Nathan Ladd <nathanladd@gmail.com>
2020-02-12 14:18:55 -0600

    Runner: Directory

--- lib/test_bench/run.rb
+++ lib/test_bench/run.rb
@@ -5,6 +5,11 @@ module TestBench
     end
     attr_writer :session
 
+    def exclude_file_pattern
+      @exclude_file_pattern ||= Defaults.exclude_file_pattern
+    end
+    attr_writer :exclude_file_pattern
+
     attr_reader :paths
 
     def initialize(*paths)
@@ -27,14 +32,36 @@ module TestBench
     end
 
     def path(path)
-      if File.exist?(path)
+      if File.directory?(path)
+        directory(path)
+      elsif File.exist?(path)
         file(path)
       end
     end
     alias_method :<<, :path
 
+    def directory(path)
+      glob_pattern = File.join(path, '**/*.rb')
+
+      Dir[glob_pattern].sort.each do |path|
+        next if exclude_file_pattern.match?(path)
+
+        file(path)
+      end
+    end
+
     def file(path)
       session.load(path)
+    end
+
+    module Defaults
+      def self.exclude_file_pattern
+        pattern = ENV.fetch('TEST_BENCH_EXCLUDE_FILE_PATTERN') do
+          '_init.rb$'
+        end
+
+        Regexp.new(pattern)
+      end
     end
   end
 end
