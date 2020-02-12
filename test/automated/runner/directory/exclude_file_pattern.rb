8350be639e6480864100ecc5b57324148ce1eae0
Nathan Ladd <nathanladd@gmail.com>
2020-02-12 14:18:55 -0600

    Runner: Directory

---
+++ test/automated/runner/directory/exclude_file_pattern.rb
@@ -0,0 +1,27 @@
+require_relative '../../automated_init'
+
+context "Runner" do
+  context "Directory" do
+    context "Exclude File Pattern" do
+      directory = Controls::Directory.example
+
+      file_path_1 = Controls::TestFile.example(filename: 'path_1.rb', directory: directory)
+      file_path_2 = Controls::TestFile.example(filename: 'path_2.rb', directory: directory)
+
+      runner = Run.new(directory)
+
+      runner.exclude_file_pattern = Regexp.new(file_path_2)
+      assert(runner.exclude_file_pattern.match?(file_path_2))
+
+      runner.()
+
+      test "Loads non-excluded file" do
+        assert(runner.session.loaded?(file_path_1))
+      end
+
+      test "Ignores excluded file" do
+        refute(runner.session.loaded?(file_path_2))
+      end
+    end
+  end
+end
