ba4072c6ebcf258245e1fce9824f5c6e8f2f4886
Nathan Ladd <nathanladd@gmail.com>
2020-03-03 16:04:14 -0600

    Output (Debug Level): Test

--- lib/test_bench/output/levels/debug.rb
+++ lib/test_bench/output/levels/debug.rb
@@ -6,11 +6,79 @@ module TestBench
 
         include Writer::Dependency
 
+        include PrintError
+
         def comment(text)
           writer
             .indent
             .text(text)
             .newline
+        end
+
+        def error(error)
+          print_error(error)
+        end
+
+        def finish_test(title, result)
+          writer.decrease_indentation
+
+          fg_color = result ? :green : :red
+
+          writer.indent
+
+          writer
+            .escape_code(fg_color)
+            .text("Finished test")
+
+          unless title.nil?
+            writer
+              .text(' ')
+              .escape_code(:bold)
+              .text(title.inspect)
+              .escape_code(:reset_intensity)
+          end
+
+          writer
+            .text(" (Result: #{result ? 'pass' : 'failure'})")
+            .escape_code(:reset_fg)
+            .newline
+        end
+
+        def skip_test(title)
+          writer.indent
+
+          writer
+            .escape_code(:yellow)
+            .text("Skipped test")
+
+          unless title.nil?
+            writer
+              .text(' ')
+              .escape_code(:bold)
+              .text(title.inspect)
+              .escape_code(:reset_intensity)
+          end
+
+          writer
+            .escape_code(:reset_fg)
+            .newline
+        end
+
+        def start_test(title)
+          if title.nil?
+            text = "Starting test"
+          else
+            text = "Starting test #{title.inspect}"
+          end
+
+          writer
+            .indent
+            .escape_code(:cyan)
+            .text(text)
+            .escape_code(:reset_fg)
+            .newline
+
+          writer.increase_indentation
         end
       end
     end
