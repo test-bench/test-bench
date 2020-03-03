2fbc3187bc15ba2bce5535db6d59d798d9dac1df
Nathan Ladd <nathanladd@gmail.com>
2020-03-03 16:44:33 -0600

    Output (Debug Level): File

---
+++ test/automated/output/debug_level/file.rb
@@ -0,0 +1,38 @@
+require_relative '../../automated_init'
+
+context "Output" do
+  context "Debug Level" do
+    context "File" do
+      output = Output::Levels::Debug.new
+
+      output.writer.enable_styling!
+
+      file_1 = Controls::TestFile.filename
+      file_2 = Controls::TestFile::Alternate.filename
+
+      result = Controls::Result.example
+
+      output.enter_file(file_1)
+
+      output.enter_context("Some Context")
+      output.exit_context("Some Context", result)
+
+      output.exit_file(file_1, result)
+
+      output.enter_file(file_2)
+      output.exit_file(file_2, result)
+
+      test do
+        assert(output.writer.written?(<<~TEXT))
+        Running #{file_1}
+        \e[32mSome Context\e[39m
+        \e[2;3;32mFinished context "Some Context" (Result: pass)\e[39;23;22m
+
+        Running #{file_2}
+        \e[2m(Nothing written)\e[22m
+
+        TEXT
+      end
+    end
+  end
+end
