51ada4fd9f2f0df3ca39f292518ca71aa77dbbbb
Nathan Ladd <nathanladd@gmail.com>
2020-03-05 19:34:32 -0600

    Output (Pass Level): Comment

---
+++ test/automated/output/pass_level/comment.rb
@@ -0,0 +1,28 @@
+require_relative '../../automated_init'
+
+context "Output" do
+  context "Pass Level" do
+    context "Comment" do
+      output = Output::Levels::Debug.new
+
+      output.writer.enable_styling!
+
+      control_fixture = Controls::Fixture.example(output)
+
+      control_fixture.instance_exec do
+        comment "Some comment"
+
+        output.writer.increase_indentation
+        comment "Other comment (indented)"
+        output.writer.decrease_indentation
+      end
+
+      test do
+        assert(output.writer.written?(<<~TEXT))
+        Some comment
+          Other comment (indented)
+        TEXT
+      end
+    end
+  end
+end
