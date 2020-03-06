276c17d04cdd1447de066491e1f906ebd15e16c6
Nathan Ladd <nathanladd@gmail.com>
2020-03-06 14:47:16 -0600

    Output (Pass Level): Build

--- lib/test_bench/output/levels/pass.rb
+++ lib/test_bench/output/levels/pass.rb
@@ -28,6 +28,16 @@ module TestBench
         end
         attr_writer :test_stack
 
+        def self.build(omit_backtrace_pattern: nil, reverse_backtraces: nil, writer: nil, styling: nil, device: nil)
+          instance = new
+
+          instance.omit_backtrace_pattern = omit_backtrace_pattern unless omit_backtrace_pattern.nil?
+          instance.reverse_backtraces = reverse_backtraces unless reverse_backtraces.nil?
+
+          Writer.configure(instance, writer: writer, styling: styling, device: device)
+          instance
+        end
+
         def comment(text)
           writer
             .indent
