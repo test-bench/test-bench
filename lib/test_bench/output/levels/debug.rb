feb1cbfcbc7a214891e3bd72c8d6f9fbffb9f03f
Nathan Ladd <nathanladd@gmail.com>
2020-03-06 14:46:13 -0600

    Output (Debug Level): Build

--- lib/test_bench/output/levels/debug.rb
+++ lib/test_bench/output/levels/debug.rb
@@ -16,6 +16,18 @@ module TestBench
         end
         attr_writer :previous_byte_offset
 
+        def self.build(omit_backtrace_pattern: nil, reverse_backtraces: nil, writer: nil, styling: nil, device: nil)
+          instance = new
+
+          instance.omit_backtrace_pattern = omit_backtrace_pattern unless omit_backtrace_pattern.nil?
+          instance.reverse_backtraces = reverse_backtraces unless reverse_backtraces.nil?
+
+          Writer.configure(instance, writer: writer, styling: styling, device: device)
+          Timer.configure(instance)
+
+          instance
+        end
+
         def comment(text)
           writer
             .indent
