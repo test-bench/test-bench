65c38485333af021041635f064700eeca29c54d3
Nathan Ladd <nathanladd@gmail.com>
2020-03-06 11:25:00 -0600

    Output (Pass Level): Test

--- lib/test_bench/output/levels/pass.rb
+++ lib/test_bench/output/levels/pass.rb
@@ -6,11 +6,78 @@ module TestBench
 
         include Writer::Dependency
 
+        include PrintError
+
+        attr_accessor :previous_error
+
         def comment(text)
           writer
             .indent
             .text(text)
             .newline
+        end
+
+        def error(error)
+          self.previous_error = error
+        end
+
+        def print_previous_error
+          print_error(previous_error)
+
+          self.previous_error = nil
+        end
+
+        def finish_test(title, result)
+          writer.indent
+
+          if writer.styling?
+            unless result && title.nil?
+              writer.escape_code(:bold) unless result
+
+              fg_color = result ? :green : :red
+
+              writer
+                .escape_code(fg_color)
+                .text(title || 'Test')
+                .escape_code(:reset_fg)
+
+              writer.escape_code(:reset_intensity) unless result
+
+              writer.newline
+            end
+          else
+            text = String.new("Finished test")
+            text << " #{title.inspect}" unless title.nil?
+            text << " (Result: #{result ? 'pass' : 'failure'})"
+
+            writer.text(text).newline
+          end
+
+          unless previous_error.nil?
+            writer.increase_indentation
+
+            print_previous_error
+
+            writer.decrease_indentation
+          end
+        end
+
+        def skip_test(title)
+          writer.indent
+
+          if writer.styling?
+            writer
+              .escape_code(:yellow)
+              .text(title || 'Test')
+              .escape_code(:reset_fg)
+
+            writer.newline
+          else
+            text = String.new("Skipped test")
+            text << " #{title.inspect}" unless title.nil?
+
+            writer.text(text).newline
+          end
         end
       end
     end
