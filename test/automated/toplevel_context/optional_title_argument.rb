5469c74696899b40200bcc562ca98959f7c728c5
Nathan Ladd <nathanladd@gmail.com>
2020-06-10 21:18:26 -0500

    Toplevel Module: Context

---
+++ test/automated/toplevel_context/optional_title_argument.rb
@@ -0,0 +1,37 @@
+require_relative '../automated_init'
+
+context "Toplevel Context" do
+  context "Optional Title Argument" do
+    context "Given" do
+      session = Session::Substitute.build
+
+      TestBench.context("Some Context", session: session) do
+        #
+      end
+
+      recorded_title = session.output.exit_context_recorded? do |title|
+        title == "Some Context"
+      end
+
+      test do
+        assert(recorded_title)
+      end
+    end
+
+    context "Omitted" do
+      session = Session::Substitute.build
+
+      TestBench.context(session: session) do
+        block_context = self
+      end
+
+      recorded_no_title = session.output.exit_context_recorded? do |title|
+        title.nil?
+      end
+
+      test do
+        assert(recorded_no_title)
+      end
+    end
+  end
+end
