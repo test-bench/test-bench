de882ae2fd5c1fb65fcf341897e4b206f3fb6e1b
Nathan Ladd <nathanladd@gmail.com>
2020-07-15 15:15:06 -0500

    Output (Raw): Detail

---
+++ test/automated/output/raw/detail/detail.rb
@@ -0,0 +1,39 @@
+require_relative '../../../automated_init'
+
+context "Raw Output" do
+  context "Detail" do
+    context "Detail Setting: On" do
+      output = Output::Raw.new
+
+      output.detail_setting = :on
+
+      control_fixture = Controls::Fixture.example(output)
+
+      control_fixture.instance_exec do
+        detail "Some detail"
+      end
+
+      test do
+        assert(output.writer.written?(<<~TEXT))
+        Some detail
+        TEXT
+      end
+    end
+
+    context "Detail Setting: Off" do
+      output = Output::Raw.new
+
+      output.detail_setting = :off
+
+      control_fixture = Controls::Fixture.example(output)
+
+      control_fixture.instance_exec do
+        detail "Some detail"
+      end
+
+      test do
+        refute(output.writer.written?)
+      end
+    end
+  end
+end
