e5e9ba1dc0cf1b6b4f2ece114b61a1a629887f8c
Nathan Ladd <nathanladd@gmail.com>
2020-07-15 15:15:06 -0500

    Output (Raw): Detail

---
+++ test/automated/output/raw/detail/styling.rb
@@ -0,0 +1,73 @@
+require_relative '../../../automated_init'
+
+context "Raw Output" do
+  context "Detail" do
+    context "Styling" do
+      context "Non Verbose" do
+        output = Output::Raw.new
+
+        output.verbose = false
+
+        output.writer.enable_styling!
+
+        control_fixture = Controls::Fixture.example(output)
+
+        control_fixture.instance_exec do
+          detail "Some detail"
+        end
+
+        test do
+          assert(output.writer.written?(<<TEXT))
+Some detail
+TEXT
+        end
+      end
+
+      context "Verbose" do
+        context "Detail Setting: On" do
+          output = Output::Raw.new
+
+          output.verbose = true
+
+          output.detail_setting = :on
+
+          output.writer.enable_styling!
+
+          control_fixture = Controls::Fixture.example(output)
+
+          control_fixture.instance_exec do
+            detail "Some detail"
+          end
+
+          test do
+            assert(output.writer.written?(<<TEXT))
+Some detail
+TEXT
+          end
+        end
+
+        context "Detail Setting: Off" do
+          output = Output::Raw.new
+
+          output.verbose = true
+
+          output.detail_setting = :off
+
+          output.writer.enable_styling!
+
+          control_fixture = Controls::Fixture.example(output)
+
+          control_fixture.instance_exec do
+            detail "Some detail"
+          end
+
+          test do
+            assert(output.writer.written?(<<TEXT))
+(detail omitted: Some detail)
+TEXT
+          end
+        end
+      end
+    end
+  end
+end
