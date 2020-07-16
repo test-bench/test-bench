fd2ca730a32b47baa2f89a90fae61295edd080b7
Nathan Ladd <nathanladd@gmail.com>
2020-07-16 15:54:40 -0500

    Output (Raw): Detail Setting

--- lib/test_bench/output/raw.rb
+++ lib/test_bench/output/raw.rb
@@ -1,6 +1,8 @@
 module TestBench
   module Output
     class Raw
+      Error = Class.new(RuntimeError)
+
       def verbose
         instance_variable_defined?(:@verbose) ?
           @verbose :
@@ -8,6 +10,11 @@ module TestBench
       end
       attr_writer :verbose
       alias_method :verbose?, :verbose
+
+      def detail_setting
+        @detail_setting ||= Defaults.detail
+      end
+      attr_writer :detail_setting
 
       attr_accessor :current_batch
 
@@ -23,9 +30,50 @@ module TestBench
         end
       end
 
+      def detail?(result=nil)
+        result ||= current_batch&.result
+
+        case detail_setting
+        when :failure
+          result != true
+        when :on
+          true
+        when :off
+          false
+        end
+      end
+
+      def self.assure_detail_setting(detail_setting)
+        unless detail_settings.include?(detail_setting)
+          raise Error, "Invalid detail setting #{detail_setting.inspect} (Valid values: #{detail_settings.map(&:inspect).join(', ')})"
+        end
+      end
+
+      def self.detail_settings
+        [
+          :failure,
+          :on,
+          :off
+        ]
+      end
+
+      def self.default_detail_setting
+        detail_settings.fetch(0)
+      end
+
       module Defaults
         def self.verbose
           Environment::Boolean.fetch('TEST_BENCH_VERBOSE', false)
+        end
+
+        def self.detail
+          detail = ::ENV['TEST_BENCH_DETAIL']
+
+          if detail.nil?
+            Raw.default_detail_setting
+          else
+            detail.to_sym
+          end
         end
       end
     end
