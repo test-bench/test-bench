4bff50c10d7d694631155ba217ff9449f3479004
Nathan Ladd <nathanladd@gmail.com>
2020-07-21 15:51:28 -0500

    Output (Batch Data): Toplevel

--- lib/test_bench/output/batch_data.rb
+++ lib/test_bench/output/batch_data.rb
@@ -1,12 +1,16 @@
 module TestBench
   module Output
-    BatchData = Struct.new(:result) do
+    BatchData = Struct.new(:result, :depth) do
       def passed?
         result
       end
 
       def failed?
         !result
+      end
+
+      def toplevel?
+        depth.zero?
       end
     end
   end
