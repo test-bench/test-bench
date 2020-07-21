35a557647861ecf40e5e7c94b23164f959743f64
Nathan Ladd <nathanladd@gmail.com>
2020-07-21 17:12:37 -0500

    Output (Raw): Current Batch

--- lib/test_bench/output/raw.rb
+++ lib/test_bench/output/raw.rb
@@ -9,6 +9,20 @@ module TestBench
       attr_writer :verbose
       alias_method :verbose?, :verbose
 
+      attr_accessor :current_batch
+
+      def batch_starting(batch_data)
+        if batch_data.depth.zero?
+          self.current_batch = batch_data
+        end
+      end
+
+      def batch_finished(batch_data)
+        if batch_data.depth.zero?
+          self.current_batch = nil
+        end
+      end
+
       module Defaults
         def self.verbose
           Environment::Boolean.fetch('TEST_BENCH_VERBOSE', false)
