abbc662b619dad2b7b630a575f48b40cfe590205
Nathan Ladd <nathanladd@gmail.com>
2023-07-16 12:17:31 -0500

    CLI: Actuate

--- lib/test_bench/cli.rb
+++ lib/test_bench/cli.rb
@@ -1,5 +1,35 @@
 module TestBench
   module CLI
+    def self.call(...)
+      path_arguments = ParseArguments.(...)
+
+      default_path = Defaults.tests_directory
+
+      result = Run.! do |run|
+        if not STDIN.tty?
+          until STDIN.eof?
+            path = STDIN.gets(chomp: true)
+
+            next if path.empty?
+
+            run << path
+          end
+        end
+
+        path_arguments.each do |path|
+          run << path
+        end
+
+        if not run.ran?
+          run << default_path
+        end
+      end
+
+      exit_code = exit_code(result)
+
+      exit(exit_code)
+    end
+
     def self.exit_code(result)
       if result == true
         0
@@ -7,6 +37,12 @@ module TestBench
         1
       else
         2
+      end
+    end
+
+    module Defaults
+      def self.tests_directory
+        ParseArguments::Defaults.tests_directory
       end
     end
   end
