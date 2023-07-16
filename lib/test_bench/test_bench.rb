c2dba0623c29e9acbeee1b38cf5b720d112233cb
Nathan Ladd <nathanladd@gmail.com>
2023-07-16 10:32:42 -0500

    Toplevel Namespace: Evaluate

--- lib/test_bench/test_bench.rb
+++ lib/test_bench/test_bench.rb
@@ -1,5 +1,23 @@
 module TestBench
+  def self.evaluate(session: nil, &block)
+    fixture = TestBench::Fixture::Evaluate.build(session:, &block)
+    fixture.extend(DeactivatedVariants)
+    fixture.()
+
+    fixture.test_session.passed?
+  end
+
   def self.session
     Session::Store.get
+  end
+
+  module DeactivatedVariants
+    def _context(title=nil, &)
+      context(title)
+    end
+
+    def _test(title=nil, &)
+      test(title)
+    end
   end
 end
