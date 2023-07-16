2f6cd7fb1a3601c365c9e0d94ebc18d7164c0329
Nathan Ladd <nathanladd@gmail.com>
2023-07-16 10:56:01 -0500

    Toplevel Namespace: Context

--- lib/test_bench/test_bench.rb
+++ lib/test_bench/test_bench.rb
@@ -1,4 +1,12 @@
 module TestBench
+  def self.context(title=nil, session: nil, &block)
+    evaluate(session:) do
+      context(title) do
+        instance_exec(&block)
+      end
+    end
+  end
+
   def self.evaluate(session: nil, &block)
     fixture = TestBench::Fixture::Evaluate.build(session:, &block)
     fixture.extend(DeactivatedVariants)
